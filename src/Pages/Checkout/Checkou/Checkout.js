c91436822f5c7fc6c94eb539125ce3cefa96e1a0
ahmednayeeme <ahmednaim353@gmail.com>
2022-04-28 01:22:41 +0600

    reload problem solves

--- src/Pages/Checkout/Checkou/Checkout.js
+++ src/Pages/Checkout/Checkou/Checkout.js
@@ -3,8 +3,8 @@ import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div className='checkout w-25 mx-auto mt-5'>
-            <h2 className='fs-1 p-2'>Your Booking Confirm</h2>
+        <div className='checkout mt-5'>
+            <h2 className='fs-2 p-2'>Your Booking Confirm</h2>
         </div>
     );
 };
